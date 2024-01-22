d81a30f537afc308cbbfe10e6a00d1b50ec4a216
Mayank Dubey <mayank.dubey@appinventiv.com>
2024-01-22 18:41:05 +0530

    assignment 3

--- lib/src/presentation/ui/assignment_selection.dart
+++ lib/src/presentation/ui/assignment_selection.dart
@@ -1,6 +1,6 @@
 import 'package:annimation_assignment/src/presentation/ui/assignment%202.dart';
+import 'package:annimation_assignment/src/presentation/ui/assignment_3.dart';
 import 'package:flutter/material.dart';
-
 import 'login_screen/login_screen.dart';
 
 class AssignmentSelection extends StatefulWidget{
@@ -23,7 +23,10 @@ class _AssignmentSelectionState extends State<AssignmentSelection> {
           ),
           ElevatedButton(onPressed: (){
             Navigator.push(context, MaterialPageRoute(builder: (context)=>Assignment2()));
-          }, child:Text("Assignment 2") )
+          }, child:Text("Assignment 2") ),
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>Assignment3()));
+          }, child:Text("Assignment 3") )
 
         ],
       ),
